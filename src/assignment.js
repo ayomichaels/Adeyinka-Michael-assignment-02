518d36e10dce15d4806b03d98776dbd8fd385e76
Ayo Michaels <90911968+ayomichaels@users.noreply.github.com>
2022-07-05 15:57:03 +0100

    assignment completed

--- src/assignment.js
+++ src/assignment.js
@@ -8,7 +8,7 @@ const assignment = {};
  * Challenge - 1
  * 
  * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
+ * If you did Challenge - 1, remove the comment in the lie just after this function
  * 
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
